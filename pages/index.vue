dacf23c96591d0459389ef32278e0361378da2e3
dann4624 <43332913+dann4624@users.noreply.github.com>
2021-03-24 11:15:24 +0100

    Initial

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>
+      Welcome to the best corny dad jokes APP on the web.
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+    head(){
+        return {
+            title: "Welcome to DadJokes",
+            meta: [
+                {       
+                    hid: "description",         
+                    name: "description",
+                    content: "Best place for corny dad jokes"
+                }
+
+            ]
+        };
+    }
+};
+</script>
